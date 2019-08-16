fe92746cf953656cf372f59d43d93ba9495964e3
github username <github email address>
2019-08-16 15:32:59 +0100

    started api setup

---
+++ react_pokemobs/adapters/API.js
@@ -0,0 +1,6 @@
+const pokemonAPI = `https://pokeapi.co/api/v2/pokemon/${name}`;
+const generationAPI = "https://pokeapi.co/api/v2/generation/1"
+// const jsonify = resp => {
+//   if (resp.ok) return resp.json();
+//   else throw new Error(resp.json());
+// };
